002a5d25c76163564a77947cf136391d1ded3277
Omisola Idowu Paul <57432464+omisolaidowu@users.noreply.github.com>
2023-07-16 01:00:41 +0100

    updated code

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ app = FastAPI()
 class HelloWorld():
     def read_hello(self):
         return {"message": "Hello World"}
+    
+hello_world = HelloWorld()
 router = APIRouter()
 router.add_api_route('/api/v2/hello-world', 
-endpoint = HelloWorld().read_hello, methods=["GET"])
+endpoint = hello_world.read_hello, methods=["GET"])
 app.include_router(router)
 
-# if __name__ == "__main__":
-#    uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)
+if __name__ == "__main__":
+   uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)
 
 
