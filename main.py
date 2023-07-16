2c9be3621386e75bfa48804b59539e6a4f15c838
Omisola Idowu Paul <57432464+omisolaidowu@users.noreply.github.com>
2023-07-16 01:08:36 +0100

    updated route

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ class HelloWorld():
         return {"message": "Hello World"}
     
 hello_world = HelloWorld()
-router = APIRouter()
-router.add_api_route('/api/v2/hello-world', 
+route = APIRouter()
+route.add_api_route('/api/v2/hello-world', 
 endpoint = hello_world.read_hello, methods=["GET"])
-app.include_router(router)
+app.include_router(route)
 
 if __name__ == "__main__":
    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
